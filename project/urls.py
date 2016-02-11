7824bd1724125c8716d55f1b0a484b5df98360c6
Alex Clark <aclark@aclark.net>
2016-02-11 08:45:35 -0500

    Update

--- project/urls.py
+++ project/urls.py
@@ -10,10 +10,13 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = [url(r'^admin/', admin.site.urls), ]
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
