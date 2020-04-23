6cb8663abfc0b219dd3e0526e456c10f02a5040c
Alex Clark <aclark@aclark.net>
2020-04-23 12:06:46 -0400

    default list-targets

--- base.mk
+++ base.mk
@@ -218,7 +218,7 @@ else
 	@echo "Unsupported"
 endif
 
-list-targets:
+list-targets-default:
 	@$(MAKE) -pRrq -f $(lastword $(MAKEFILE_LIST)) : 2>/dev/null | awk -v RS= -F:\
         '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}'\
         | sort | egrep -v -e '^[^[:alnum:]]' -e '^$@$$' | xargs | tr ' ' '\n' | awk\
