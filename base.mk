955429a9591d93e49e1923075cff7adba89abdce
Alex Clark <aclark@aclark.net>
2020-10-26 18:34:47 -0400

    Add Py 3.9

--- base.mk
+++ base.mk
@@ -330,6 +330,8 @@ python-virtualenv-3-7-default:
 	virtualenv --python=python3.7 .
 python-virtualenv-3-8-default:
 	virtualenv --python=python3.8 .
+python-virtualenv-3-9-default:
+	virtualenv --python=python3.8 .
 #
 # Sphinx
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
