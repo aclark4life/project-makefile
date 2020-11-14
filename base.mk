3062cb46fe0f9351189058777e6ccaac03125130
Alex Clark <aclark@aclark.net>
2020-11-14 17:07:29 -0500

    Update

--- base.mk
+++ base.mk
@@ -319,6 +319,7 @@ pip-install-sphinx:
 	$(MAKE) pip-install
 pip-upgrade-default:
 	cat requirements.txt | awk -F \= '{print $1}' > $(TMPDIR)/requirements.txt
+	cat $(TMPDIR)/requirements.txt
 	mv -f $(TMPDIR)/requirements.txt .
 	$(PIP) install -U -r requirements.txt
 	$(MAKE) pip-freeze
