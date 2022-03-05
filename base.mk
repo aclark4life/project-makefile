c571ea5a7e249276c19557ec12ef6fed1f24cd71
Alex Clark <aclark@aclark.net>
2022-03-05 09:11:39 -0500

    Update

--- base.mk
+++ base.mk
@@ -195,7 +195,7 @@ urlpatterns = urlpatterns + [
 endef
 export HOME_PAGE
 export JENKINS_FILE
-export DJANGO_URLS
+export API_AUTH
 
 # Rules
 # ------------------------------------------------------------------------------  
@@ -309,7 +309,7 @@ django-user-default:
 
 django-urls-default:
 	echo "\n# $(PROJECT_NAME)\n" >> $(PROJECT_NAME)/$(URLS)
-	echo "$(URLS)\n" >> $(PROJECT_NAME)/$(URLS)
+	echo "$(API_AUTH)\n" >> $(PROJECT_NAME)/$(URLS)
 
 django-npm-install-default:
 	cd frontend; npm install
