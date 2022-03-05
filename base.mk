ad7e02a122a2f8e880b70c056f8d07afd1ba3fc7
Alex Clark <aclark@aclark.net>
2022-03-05 09:38:33 -0500

    Update

--- base.mk
+++ base.mk
@@ -165,7 +165,11 @@ define HOME_PAGE
     <h1 class="display-3">Hello, world!</h1>
     <p>This is a template for a simple marketing or informational website. It includes a large callout called a
       jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-    <p><a class="btn btn-primary btn-lg" href="{% url 'admin:index' %}" role="button">Learn more »</a></p>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <a type="button" class="btn btn-primary" href="{% url 'admin:index' %}" role="button">Learn more »</a>
+      <button type="button" class="btn btn-primary">Middle</button>
+      <button type="button" class="btn btn-primary">Right</button>
+    </div>
     <div class="d-flex justify-content-center">
       <img src="{% static 'vendors/images/webpack.png' %}" class="img-fluid"/>
     </div>
