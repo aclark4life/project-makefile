7089d3fa0581e2d2e55b8d8a4dbd9ee5583a2275
Alex Clark <aclark@aclark.net>
2023-11-22 09:11:12 -0500

    Update

--- base.mk
+++ base.mk
@@ -558,7 +558,7 @@ flake-default:
 	-flake8 $(PROJECT_NAME)/*.py
 	-flake8 $(PROJECT_NAME)/*/*.py
 
-help-default:  # Via https://chat.openai.com/share/8f2ca244-b6e4-4af3-81e2-714726b6cf8b
+help-default:  # Via https://chat.openai.com/share/8f2ca244-b6e4-4af3-81e2-714726b6cf8b, http://stackoverflow.com/a/26339924
 	@for makefile in $(MAKEFILE_LIST); do \
         $(MAKE) -pRrq -f $$makefile : 2>/dev/null \
             | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' \
