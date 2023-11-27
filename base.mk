e5ef883d64fa673982b3853702423793527ec83b
Alex Clark <aclark@aclark.net>
2023-11-26 19:56:11 -0500

    Update

--- base.mk
+++ base.mk
@@ -342,7 +342,7 @@ define HOME_PAGE_TEMPLATE
                 <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z" />
             </svg>
             <div>
-                <h2>{% trans "Tutorial" %}</h2>
+                <h2>Wagtail Tutorial</h2>
                 <p>{% trans "Build your first Wagtail site" %}</p>
             </div>
         </a>
