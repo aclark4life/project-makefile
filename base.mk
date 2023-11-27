b20e1c912d85fb65bf384025fd4ede84ead547a8
Alex Clark <aclark@aclark.net>
2023-11-26 19:54:51 -0500

    Update

--- base.mk
+++ base.mk
@@ -346,6 +346,19 @@ define HOME_PAGE_TEMPLATE
                 <p>{% trans "Build your first Wagtail site" %}</p>
             </div>
         </a>
+        <a target="_blank" class="option option-two"
+           href="https://docs.djangoproject.com/en/4.2/intro/tutorial01/">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24"
+                 aria-hidden="true">
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z" />
+            </svg>
+            <div>
+                <h2>Django Tutorial</h2>
+                <p>{% trans "Build your first Wagtail site" %}</p>
+            </div>
+        </a>
         <a target="_blank"
            class="option option-three"
            href="{% url 'wagtailadmin_home' %}">
