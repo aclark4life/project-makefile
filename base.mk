caa4244d21c44ef15ccb692aefa5f648955d8161
Alex Clark <aclark@aclark.net>
2023-12-03 14:26:38 -0500

    Update

--- base.mk
+++ base.mk
@@ -757,8 +757,12 @@ pg-init-default:
 pip-freeze-default:
 	pip3 freeze | sort > $(TMPDIR)/requirements.txt
 	mv -f $(TMPDIR)/requirements.txt .
-	-git add requirements.txt
-	-git commit -a -m "Freezing requirements."
+    @if [ "$(GIT_COMMANDS)" != "false" ]; then \
+		-git add requirements.txt \
+		-git commit -a -m "Freezing requirements." \
+    else \
+        echo "Git commands are skipped as GIT_COMMANDS is set to false"; \
+    fi
 
 pip-install-default:
 	$(MAKE) pip-upgrade
