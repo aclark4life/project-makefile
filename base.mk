b080a79c941059756b2339d6ec5158ad2cb2d158
Alex Clark <aclark@aclark.net>
2023-12-07 14:26:34 -0500

    Update

--- base.mk
+++ base.mk
@@ -151,12 +151,6 @@ define BASE_TEMPLATE
 				<li class="nav-item">
 				  <a class="nav-link active" aria-current="page" href="/">Home</a>
 				</li>
-				<li class="nav-item">
-				  <a class="nav-link" href="#">Link</a>
-				</li>
-				<li class="nav-item">
-				  <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-				</li>
 			  </ul>
 			  <form class="d-flex" role="search">
 				<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
