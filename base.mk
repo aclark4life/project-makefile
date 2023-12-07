af0754e83eac555dcc9e3d78c06654b7d8859405
Alex Clark <aclark@aclark.net>
2023-12-07 14:07:45 -0500

    Update

--- base.mk
+++ base.mk
@@ -142,7 +142,7 @@ define BASE_TEMPLATE
 			<div class="collapse navbar-collapse" id="navbarCollapse">
 			  <ul class="navbar-nav me-auto mb-2 mb-md-0">
 				<li class="nav-item">
-				  <a class="nav-link active" aria-current="page" href="#">Home</a>
+				  <a class="nav-link active" aria-current="page" href="/">Home</a>
 				</li>
 				<li class="nav-item">
 				  <a class="nav-link" href="#">Link</a>
