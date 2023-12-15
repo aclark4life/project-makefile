efc04bc74619a2be33b33da2016405f0a1af820b
Alex Clark <aclark@aclark.net>
2023-12-15 12:09:55 -0500

    Update

--- base.mk
+++ base.mk
@@ -886,6 +886,7 @@ django-settings-default:
 	echo "INSTALLED_APPS.append('wagtail.contrib.settings')" >> $(SETTINGS)
 	echo "INSTALLED_APPS.append('django_extensions')" >> $(SETTINGS)
 	echo "INSTALLED_APPS.append('debug_toolbar')" >> $(DEV_SETTINGS)
+	echo "INSTALLED_APPS.append('crispy_forms')" >> $(SETTINGS)
 	echo "MIDDLEWARE.append('allauth.account.middleware.AccountMiddleware')" >> $(SETTINGS)
 	echo "MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')" >> $(DEV_SETTINGS)
 	echo "STATICFILES_DIRS.append(os.path.join(BASE_DIR, 'frontend/build'))" >> $(SETTINGS)
