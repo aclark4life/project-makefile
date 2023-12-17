c83a352d0abc94933cf923c3144fa6385b02a1a4
Alex Clark <aclark@aclark.net>
2023-12-17 14:32:42 -0500

    Update

--- base.mk
+++ base.mk
@@ -121,7 +121,16 @@ const UserMenu = ({ isAuthenticated }) => {
   return (
     <div>
       {isAuthenticated ? (
-        <button className="btn" onClick={handleLogout}>Logout</button>
+		<div className="dropdown">
+		  <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+			Dropdown button
+		  </button>
+		  <ul className="dropdown-menu">
+			<li><a className="dropdown-item" href="#">Action</a></li>
+			<li><a className="dropdown-item" href="#">Another action</a></li>
+			<li><a className="dropdown-item" href="#">Something else here</a></li>
+		  </ul>
+		</div>
       ) : (
         <a className="btn" href="/accounts/login">Login</a>
       )}
