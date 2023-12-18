77ee30e54a746415b6b859ed5bb68466bd93b8e8
Alex Clark <aclark@aclark.net>
2023-12-18 13:32:12 -0500

    Update

--- base.mk
+++ base.mk
@@ -122,9 +122,7 @@ const UserMenu = ({ isAuthenticated }) => {
     <div>
       {isAuthenticated ? (
         <div className="dropdown">
-          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown button
-          </button>
+          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"></button>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/accounts/logout">Logout</a></li>
           </ul>
@@ -688,10 +686,6 @@ define NAVBAR_HTML
                 {% endfor %}
               </ul>
               <div data-component="UserMenu" data-is-authenticated="{{ request.user.is_authenticated }}"></div>
-              <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
             </div>
           </div>
         </nav>
