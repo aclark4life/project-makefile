92617a2fb8b7a8272aeba568af5d14027766f2d1
Alex Clark <aclark@aclark.net>
2023-12-26 10:53:26 -0500

    Update

--- base.mk
+++ base.mk
@@ -499,8 +499,6 @@ define HOME_PAGE_TEMPLATE
                 {% endfor %}
             </div>
         {% endif %}
-		{{ page.body|default:""|safe }}
-		{% include_block page.carousel_block %}
 		{% for block in page.marketing_blocks %}
 		  {% include_block block %}
 		{% endfor %}
