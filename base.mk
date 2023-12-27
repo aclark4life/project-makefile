900e390aa2aceb634d12cae39d5de00e048bb93b
Alex Clark <aclark@aclark.net>
2023-12-27 11:06:42 -0500

    Update

--- base.mk
+++ base.mk
@@ -713,8 +713,9 @@ export { default as UserMenu } from './UserMenu';
 endef
 
 define HTML_FOOTER
+  {% wagtail_site as current_site %}
   <footer class="my-5 pt-5 text-center text-small">
-    <p class="mb-1">&copy; 2017–2023 Company Name</p>
+    <p class="mb-1">&copy; {% now "Y" %} {{ current_site.site_name|default:"Project Makefile" }}</p>
     <ul class="list-inline">
       <li class="list-inline-item"><a class="text-secondary text-decoration-none" href="/privacy">Privacy</a></li>
       <li class="list-inline-item"><a class="text-secondary text-decoration-none" href="/terms-of-use">Terms</a></li>
