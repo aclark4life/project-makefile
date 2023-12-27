35a7de2bdfd737d1a6d42736afa0c2ff5f89ca5d
Alex Clark <aclark@aclark.net>
2023-12-27 11:12:12 -0500

    Update

--- base.mk
+++ base.mk
@@ -718,9 +718,9 @@ define HTML_FOOTER
     {% wagtail_site as current_site %}
     <p class="mb-1">&copy; {% now "Y" %} {{ current_site.site_name|default:"Project Makefile" }}</p>
     <ul class="list-inline">
-      <li class="list-inline-item"><a class="text-secondary text-decoration-none" href="/privacy">Privacy</a></li>
-      <li class="list-inline-item"><a class="text-secondary text-decoration-none" href="/terms-of-use">Terms</a></li>
-      <li class="list-inline-item"><a class="text-secondary text-decoration-none" href="/support">Support</a></li>
+      {% for child in current_site.root_page.get_children %}
+      <li class="list-inline-item"><a class="text-secondary text-decoration-none" href="{{ child.url }}">{{ child }}</a></li>
+      {% endfor %}
     </ul>
     <div data-component="Clock"></div>
   </footer>
