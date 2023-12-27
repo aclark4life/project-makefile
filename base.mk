3bf8fec767270cc36867d87cafed0bf853ccd140
Alex Clark <aclark@aclark.net>
2023-12-27 13:55:38 -0500

    Update

--- base.mk
+++ base.mk
@@ -739,7 +739,7 @@ define HTML_OFFCANVAS
 {% load wagtailcore_tags %}
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">{{ current_site.site_name|default:"Project Makefile" }}</h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
