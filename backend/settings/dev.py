438760a16a9ce90f392604e4c005a468c1984527
Alex Clark <aclark@aclark.net>
2024-02-23 17:20:06 -0500

    Add frontend

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tep&b0abs#ll!teccxjj@#*4_e3jlmnj=9g!il@2_f=kuv+ay6"
+SECRET_KEY = "django-insecure-_rfig)4j4g79p*l^5n3y8vwr3^dp(ys^3^quk(!k!kl^ngk54i"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
@@ -16,9 +16,7 @@ try:
     from .local import *
 except ImportError:
     pass
-INSTALLED_APPS.append("debug_toolbar")
-MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
-MIDDLEWARE.append("hijack.middleware.HijackUserMiddleware")
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+MIDDLEWARE.append('hijack.middleware.HijackUserMiddleware')
+INTERNAL_IPS = ["127.0.0.1",]
