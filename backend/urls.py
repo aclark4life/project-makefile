563980919e1a16810c8be8d3a3d1763b0539d846
Alex Clark <aclark@aclark.net>
2024-03-01 16:39:14 -0500

    Add readme

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
 
 from rest_framework import routers, serializers, viewsets
 from dj_rest_auth.registration.views import RegisterView
