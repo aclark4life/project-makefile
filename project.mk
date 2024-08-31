1eaeb454b06d41f919409afade8c8df20c15a9ff
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-31 19:53:02 -0400

    Custom DJANGO_SETTINGS_DATABASE for GitHub Actions

--- project.mk
+++ project.mk
@@ -8,6 +8,13 @@ edit:
 review:
 	$(EDITOR_REVIEW) Makefile
 
+define DJANGO_SETTINGS_DATABASE
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://postgres:postgres@postgres:/postgres")
+DATABASES["default"] = dj_database_url.parse(DATABASE_URL)
+endef
+
+django-settings:
+	@echo "$$DJANGO_SETTINGS_DATABASE" >> $(PACKAGE_NAME)/settings.py
 
 test:
 	@echo "Running tests..."
